321d5384581975067645da3720f849221f73c4fd
labu4bd <labu4bd@gmail.com>
2018-06-01 18:21:45 +0200

    elgg theme with bootstrap
    login and registration page is working according their requirement.

---
+++ socia_bootstrap_theme/views/default/forms/login.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Twitter&reg; Bootstrap Theme for Elgg
+ *
+ * Converts all Elgg css elements to Twitter&reg; Bootstrap elements.  Helps 
+ * Designers create beautiful Bootstrap themes for Elgg.
+ *
+ * PHP version 5.6
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Elgg Themes
+ * @author     Shane Barron <admin@socia.us>
+ * @copyright  2017 SocialApparatus
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    1.2
+ * @link       http://socia.us
+ */
+/*$entity = $vars["entity"];
+$title = $entity->title;
+$subtitle = "";
+$image = elgg_view_entity_icon($entity, "small");
+$params = array(
+    "title" => $title,
+    "entity" => $entity,
+    "subtitle" => $subtitle,
+    "tags" => FALSE,
+);
+$body = elgg_view("assets/images/logo.png", $params);
+echo elgg_view_image_block($image,$body, array());
+*/
+//echo elgg_echo("<img src='{$vars['url']}mod/socia_bootstrap_theme/assets/images/headerImage.png' />");
+echo elgg_echo("<img src='{$vars['url']}mod/socia_bootstrap_theme/assets/images/infanziaLogo.png' />");
+echo elgg_echo("<h2 style='position:center;color:#CC1962'>Accedi</h2>");
+echo elgg_echo("<span style='color:#063A54;'>Inserisci le tue credenziali per effettuare l'accesso</span>");
+
+
+echo elgg_view_field([
+    '#type' => 'text',
+    'name' => 'username',
+    'autofocus' => true,
+    'required' => true,
+    //'#label' => elgg_echo('loginusername'),
+    'placeholder'=> 'Email',
+]);
+
+echo elgg_view_field([
+    '#type' => 'password',
+    'name' => 'password',
+    'required' => true,
+    //'#label' => elgg_echo('password'),
+    'placeholder'=> 'Password',
+]);
+//echo elgg_view_icon('eye');
+echo elgg_view('login/extend', $vars);
+
+if (isset($vars['returntoreferer'])) {
+    echo elgg_view_field([
+        '#type' => 'hidden',
+        'name' => 'returntoreferer',
+        'value' => 'true'
+    ]);
+}
+
+ob_start();
+?>
+<div class="elgg-foot">
+    <div class="checkbox checkbox-primary">
+        <label><input type="checkbox" value="">Rimani connesso</label>
+    </div>
+    <!-- <label class="mtm float-left checkbox checkbox-primary">
+        <input type="checkbox" name="persistent" value="true" />
+        <?php //echo elgg_echo('Rimani connesso'); ?>
+    </label> -->
+
+    <?php
+    //button
+    echo "<br>";
+    echo elgg_view('input/submit', 
+        array(
+            'value' => elgg_echo('Accedi')
+        )
+    );
+
+    //register or lost password
+    echo elgg_view_menu('login', array(
+        'sort_by' => 'priority',
+        'class' => 'elgg-menu-general elgg-menu-hz mtm',
+    ));
+    ?>
+</div>
+<?php
+$footer = ob_get_clean();
+elgg_set_form_footer($footer);
---
+++ socia_bootstrap_theme/views/default/forms/thewire/add.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Twitter&reg; Bootstrap Theme for Elgg
+ *
+ * Converts all Elgg css elements to Twitter&reg; Bootstrap elements.  Helps 
+ * Designers create beautiful Bootstrap themes for Elgg.
+ *
+ * PHP version 5.6
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Elgg Themes
+ * @author     Shane Barron <admin@socia.us>
+ * @copyright  2017 SocialApparatus
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    1.2
+ * @link       http://socia.us
+ */
+
+elgg_require_js('elgg/thewire');
+
+$post = elgg_extract('post', $vars);
+//$char_limit = (int)elgg_get_plugin_setting('limit', 'thewire');
+$char_limit = 0;
+
+$text = elgg_echo('post');
+if ($post) {
+	$text = elgg_echo('reply');
+}
+$chars_left = elgg_echo('thewire:charleft');
+
+$parent_input = '';
+if ($post) {
+	$parent_input = elgg_view('input/hidden', array(
+		'name' => 'parent_guid',
+		'value' => $post->guid,
+	));
+}
+
+$count_down = "<span>$char_limit</span> $chars_left";
+$num_lines = 2;
+if ($char_limit == 0) {
+	$num_lines = 3;
+	$count_down = '';
+} else if ($char_limit > 140) {
+	$num_lines = 3;
+}
+
+$post_input = elgg_view('input/longtext', array(
+	'name' => 'body',
+	'class' => 'mtm',
+	'id' => 'thewire-textarea',
+	'rows' => $num_lines,
+	'data-max-length' => $char_limit,
+));
+
+$submit_button = elgg_view('input/submit', array(
+	'value' => $text,
+	'id' => 'thewire-submit-button',
+));
+
+echo <<<HTML
+	$post_input
+<div id="thewire-characters-remaining">
+	$count_down
+</div>
+<div class="elgg-foot mts">
+	$parent_input
+	$submit_button
+</div>
+HTML;
